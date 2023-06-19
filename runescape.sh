9eaba75477571a9a7018990b65ac6b98d8577675
607ch00 <20735294+607ch00@users.noreply.github.com>
2023-06-19 16:36:39 -0400

    Added clear cache action to desktop file. Directly start game instead of calling another bash script

--- runescape.sh
+++ runescape.sh
@@ -2,17 +2,24 @@
 
 cd "${0%/*}"
 
-# Uncomment for audio fix if needed (other common value could be 200)
-# export PULSE_LATENCY_MSEC=500
+export PULSE_LATENCY_MSEC=200
+export PULSE_PROP_OVERRIDE="application.name='RuneScape' application.icon_name='runescape' media.role='game'"
+export __GL_SHADER_DISK_CACHE_PATH=${HOME}/'Jagex'
+export __GL_THREADED_OPTIMIZATIONS='1'
+export SDL_VIDEO_X11_WMCLASS="RuneScape"
 
-# Uncomment for NVIDIA settings https://runescape.wiki/w/Linux#NVIDIA
-# export __GL_SHADER_DISK_CACHE_PATH=${HOME}/Jagex
-# export __GL_THREADED_OPTIMIZATIONS='1'
-
+# Jagex Launcher Auth
 export JX_ACCESS_TOKEN="${JX_ACCESS_TOKEN}"
 export JX_CHARACTER_ID="${JX_CHARACTER_ID}"
 export JX_DISPLAY_NAME="${JX_DISPLAY_NAME}"
 export JX_REFRESH_TOKEN="${JX_REFRESH_TOKEN}"
 export JX_SESSION_ID="${JX_SESSION_ID}"
 
-/usr/bin/runescape-launcher
+configURI=https://www.runescape.com/k=5/l=\$\(Language:0\)/jav_config.ws
+
+# Some older graphics drivers may require this to be uncommented, but it will restrict the available graphics settings and performance.
+#export MESA_GL_VERSION_OVERRIDE=3.0
+
+unset XMODIFIERS
+
+/usr/share/games/runescape-launcher/runescape --configURI $configURI $@
